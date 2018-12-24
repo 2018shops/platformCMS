ec75b79fae8063e22c412cec7878bd9613cded35
newfulin <1289938284@qq.com>
2018-12-24 23:11:26 +0800

    分类

--- app/Admin/Controllers/Goods/GoodsClassifyController.php
+++ app/Admin/Controllers/Goods/GoodsClassifyController.php
@@ -2,61 +2,56 @@
 namespace App\Admin\Controllers\Goods;
 
 use App\Admin\Contracts\Facades\Admin;
-use App\Admin\Contracts\Grid;
-use App\Admin\Fields\CustomerButton;
 use App\Admin\Models\GoodsClassify;
 use App\Http\Controllers\Controller;
 use Encore\Admin\Controllers\ModelForm;
 use Encore\Admin\Form;
-use App\Admin\Fields\ExcelExporter\ExportTaskRecord;
 use Encore\Admin\Layout\Content;
-use App\Admin\Fields\ExtendButton;
-use App\Modules\WorkFlow\WorkFlow;
+use Encore\Admin\Tree;
 
+class GoodsClassifyController extends Controller {
+	use ModelForm;
 
-class GoodsClassifyController extends Controller
-{
-    use ModelForm;
-    public function index()
-    {
-        return Admin::content(function (Content $content){
-            $content->header('商品分类');
-            $content->description('商品分类');
-            $content->body($this->grid());
-        });
-    }
-    public function create()
-    {
-        return Admin::content(function (Content $content){
-            $content->header('新增商品分类');
-            $content->description('新增商品分类');
-            $content->body($this->form());
-        });
-    }
-    public function edit($id)
-    {
-        return Admin::content(function(Content $content) use ($id) {
-            $content->header('编辑商品分类');
-            $content->description('编辑商品分类');
-            $content->body($this->form()->edit($id));
-        });
-    }
-    public function grid()
-    {
-        return Admin::grid(GoodsClassify::class,function(Grid $grid) {
-            $grid->exporter(new ExportTaskRecord());
-            $grid->column('id','ID');
-            $grid->column('name','菜单名称');
-            $grid->column('pid','父ID');
-            $grid->column('sort','排序');
-            $grid->column('status','状态');
-            $grid->column('img','图片')->display(function($img){
-                if($img)
-                    return "<img style=\"-webkit-user-select: none;\" height=\"56\" width=\"56\"  src=\"".$img."\"";
-                else
-                    return "";
-             });
-            $grid->column('update_at','时间');
-        });
-    }
+	public function index() {
+		return Admin::content(function (Content $content) {
+			$content->header('商品分类');
+			$content->description('商品分类');
+			$content->body(GoodsClassify::tree());
+		});
+	}
+	public function create() {
+		return Admin::content(function (Content $content) {
+			$content->header('新增商品分类');
+			$content->description('新增商品分类');
+			$content->body($this->form());
+		});
+	}
+	public function edit($id) {
+		return Admin::content(function (Content $content) use ($id) {
+			$content->header('编辑商品分类');
+			$content->description('编辑商品分类');
+			$content->body($this->form()->edit($id));
+		});
+	}
+
+	public function form() {
+		return Admin::form(GoodsClassify::class, function (Form $form) {
+			$form->hidden('id');
+
+			$form->select('pid', '商品分类')
+				->options(function ($id) {
+					return GoodsClassify::selectOptions();
+				});
+
+			$form->text('name', '分类名称');
+			$form->text('sort', '排序');
+			// $form->text('status', '状态');
+			$form->image('img', '图片');
+
+			$form->saving(function (Form $form) {
+
+			});
+		});
+	}
+
 }
--- app/Admin/Models/GoodsClassify.php
+++ app/Admin/Models/GoodsClassify.php
@@ -2,30 +2,57 @@
 
 namespace App\Admin\Models;
 
+use Encore\Admin\Traits\AdminBuilder;
+use Encore\Admin\Traits\ModelTree;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
 
-class GoodsClassify extends Model
-{
-    protected $table = "goods_classify";
-    protected $primaryKey = 'id';
-    protected $keyType = 'string';
-    public $timestamps = true;
-    const CREATED_AT = 'create_time';
-    const UPDATED_AT = 'update_time';
+class GoodsClassify extends Model {
+	use ModelTree, AdminBuilder;
 
-    public static function getGoodsClassifyList($id){
-        $seleceOption = [];
-        if($id){
-            $options = DB::table('goods_classify')->select('id','name')->find($id);
-            $seleceOption[$options->id] = $options->name;;
-        }else{
-            $options = DB::table('goods_classify')->select('id','name as text')->get();
-        
-            foreach($options as $option){
-                $seleceOption[$option->id] = $option->text;
-            }
-        }
-        return $seleceOption;
-    }
+	protected $table = "goods_classify";
+	protected $primaryKey = 'id';
+	protected $keyType = 'int';
+	public $timestamps = true;
+	const CREATED_AT = 'create_time';
+	const UPDATED_AT = 'update_time';
+
+	protected $fillable = ['id', 'name', 'pid', 'sort'];
+
+	public function __construct(array $attributes = []) {
+		parent::__construct($attributes);
+
+		$this->setParentColumn('pid');
+		$this->setOrderColumn('sort');
+		$this->setTitleColumn('name');
+	}
+
+	public static function selectOptions() {
+		$options = DB::table('goods_classify')->select('id', 'name as text')
+			->where('pid', '=', 0)
+			->get();
+
+		foreach ($options as $option) {
+			$seleceOption[$option->id] = $option->text;
+		}
+
+		return $seleceOption;
+	}
+
+	public static function getGoodsClassifyList($id) {
+		$seleceOption = [];
+		if ($id) {
+			$options = DB::table('goods_classify')->select('id', 'name')->find($id);
+			$seleceOption[$options->id] = $options->name;
+		} else {
+			$options = DB::table('goods_classify')->select('id', 'name as text')
+				->where('pid', '!=', 0)
+				->get();
+
+			foreach ($options as $option) {
+				$seleceOption[$option->id] = $option->text;
+			}
+		}
+		return $seleceOption;
+	}
 }
